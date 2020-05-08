790c6a6cc138ca693a51e55ccc63e27eb0aff9b4
SwampApe <rothvann2000@gmail.com>
2020-05-07 18:14:03 -0700

    Missing css causing hidden elements to show

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tabs from './components/tabjs/Tabs.js';
 import Tab from './components/tabjs/Tab.js';
 import TabDefault from './components/tabjs/TabDefault.js';
+import CustomComponent from './components/tabjs/CustomComponent.js';
+import './style/tabjs.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Tabs>
-            <Tab label="asdf">
-                <a>nerd tab</a>
+            <Tab label="Tabjs">
+                <a></a>
+                <a></a>
             </Tab>
-            <Tab label="test">
-                <a>tab kjghkhjk</a>
+            <Tab label="Usage">
+                <a>{'<Tab label="Tab Name">'}</a>
+                <a>{String.fromCharCode(9) + '<a>Content</a>'}</a>
             </Tab>
-            <Tab label="cdasf">
-                <a>tab asdfasdf</a>
+            <Tab>
+                <CustomComponent/>
             </Tab>
             <TabDefault>
-                <a>default tabb lole</a>
+                <CustomComponent/>
             </TabDefault>
         </Tabs>
       </header>
